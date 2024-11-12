e714ca552c7c8a7c9c50dc973cf66ddd8eab6b56
ashtondsouza <ashtonpd32@gmail.com>
2024-11-12 16:33:23 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      css: {
+        additionalData: `
+          @import "~bootstrap/dist/css/bootstrap.min.css";
+        `
+      }
+    }
+  }
 })
